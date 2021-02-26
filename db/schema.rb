d9b8108868ef6ec8d04fbb2579e82e043dc66d02
vagrant <vagrant@localhost.localdomain>
2021-02-26 12:57:32 +0000

    [add]post/show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_074244) do
+ActiveRecord::Schema.define(version: 2021_02_26_092433) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_074244) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
