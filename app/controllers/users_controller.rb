d4bc43f95fd2a97044928509103fc7d64c3b579f
vagrant <vagrant@localhost.localdomain>
2021-02-27 06:42:59 +0000

    [add]users/show,edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,15 +18,16 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		@user =User.find(params[:id])
+		@user = User.find(params[:id])
 		if @user.update(user_params)
 			redirect_to user_path(@user)
 		else
 			render :edit
 		end
 	end
+
 	private
 	def user_params
-		params.require(:user).permit(:name, :profile_image)
+		params.require(:user).permit(:name, :profile, :email, :profile_image)
 	end
-	end
+end
