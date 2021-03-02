561261804a04242d105d054caa6c1b887ed5c890
vagrant <vagrant@localhost.localdomain>
2021-03-02 10:08:26 +0000

    [add]slider

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def after_sign_in_path_for(resource)
-		user_path(current_user)
+		posts_path
 	end
 
 	def after_sign_out_path_for(resource)
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
 
 	private
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 	end
 end
