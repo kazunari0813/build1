16414b1dd3127402fdd91e527c661541269a4089
vagrant <haru1529@icloud.com>
2021-06-05 11:55:29 +0000

    [fix]message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,19 @@ class MessagesController < ApplicationController
 	before_action :authenticate_user!
 
   def create
-    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
+    if current_user.entries.where(room_id: params[:message][:room_id]).present?
+      @message = Message.create(message_params_with_current_user_id)
     else
       flash[:alert] = "メッセージ送信に失敗しました。"
     end
-		redirect_to "/rooms/#{@message.room_id}"
+		redirect_to room_path(@message.room_id)
+  end
+
+  private
+  def message_params
+  	params.require(:message).permit(:user_id, :message, :room_id)
+  end
+  def message_params_with_current_user_id
+  	message_params.merge(user_id: current_user.id)
   end
 end
